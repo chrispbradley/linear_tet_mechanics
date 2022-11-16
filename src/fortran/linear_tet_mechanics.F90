3f9454fb952ce27ee573648ee40bbc4f4413435e
Chris Bradley <c.bradley@auckland.ac.nz>
2022-11-17 11:37:09 +1300

    Backup

--- src/fortran/linear_tet_mechanics.F90
+++ src/fortran/linear_tet_mechanics.F90
@@ -20,6 +20,7 @@ PROGRAM LinearTetMechanicsExample
 
   !Test program parameters
 
+  INTEGER(CMISSIntg), PARAMETER :: ContextUserNumber=1
   INTEGER(CMISSIntg), PARAMETER :: CoordinateSystemUserNumber=1
   INTEGER(CMISSIntg), PARAMETER :: NumberOfSpatialCoordinates=3
   INTEGER(CMISSIntg), PARAMETER :: RegionUserNumber=1
@@ -66,7 +67,7 @@ PROGRAM LinearTetMechanicsExample
   INTEGER(CMISSIntg) :: decompositionIndex,EquationsSetIndex  
   INTEGER(CMISSIntg) :: NumberOfComputationalNodes,ComputationalNodeNumber
 
-  !CMISS variables
+  !OpenCMISS variables
 
   TYPE(cmfe_BasisType) :: Basis
   TYPE(cmfe_BoundaryConditionsType) :: BoundaryConditions
@@ -110,17 +111,18 @@ PROGRAM LinearTetMechanicsExample
   IF(.NOT.QUICKWIN_STATUS) QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
 #endif
 
-  !Intialise cmiss
-  CALL cmfe_Context_Initialise(context,err)
-  CALL cmfe_Initialise(context,err)
+  !Intialise OpenCMISS
+  CALL cmfe_Initialise(err)
   CALL cmfe_ErrorHandlingModeSet(CMFE_ERRORS_TRAP_ERROR,err)
+  !Set all diganostic levels on for testing
+  CALL cmfe_DiagnosticsSetOn(CMFE_FROM_DIAG_TYPE,[1,2,3,4,5],"Diagnostics",["FiniteElasticity_FiniteElementResidualEvaluate"],Err) !CMFE_ALL_DIAG_TYPE
+  !Create a context
+  CALL cmfe_Context_Initialise(context,err)
+  CALL cmfe_Context_Create(ContextUserNumber,context,err)
   CALL cmfe_Region_Initialise(worldRegion,err)
   CALL cmfe_Context_WorldRegionGet(context,worldRegion,err)
 
   WRITE(*,'(A)') "Program starting."
-
-  !Set all diganostic levels on for testing
-  CALL cmfe_DiagnosticsSetOn(CMFE_FROM_DIAG_TYPE,[1,2,3,4,5],"Diagnostics",["FiniteElasticity_FiniteElementResidualEvaluate"],Err) !CMFE_ALL_DIAG_TYPE
 
   !Get the number of computational nodes and this computational node number
   CALL cmfe_ComputationEnvironment_Initialise(computationEnvironment,err)
@@ -411,7 +413,10 @@ PROGRAM LinearTetMechanicsExample
   CALL cmfe_Fields_ElementsExport(Fields,"./results/LinearTet","FORTRAN",Err)
   CALL cmfe_Fields_Finalise(Fields,Err)
 
-  CALL cmfe_Finalise(context,Err)
+  !Destroy the context
+  CALL cmfe_Context_Destroy(context,err)
+  !Finalise OpenCMISS
+  CALL cmfe_Finalise(err)
 
   WRITE(*,'(A)') "Program successfully completed."
 
